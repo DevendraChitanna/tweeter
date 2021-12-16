74b631c5c46554fd6fadfdca24ce1232cf59c097
DevendraChitanna <devendrachitanna@gmail.com>
2021-12-15 20:17:39 -0500

    tweet container created and styled, nav bar fixed to the top of page

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  // --- our code goes here ---
+  // alert('The DOM is loaded')
+  $('#tweet-text').on('keyup', (e) => {
+    // console.log("event ===>", e)
+    // console.log("target  ===>", e.target)
+    console.log("value ===>", e.target.value)
+    // console.log("length ===>", e.target.value.length)
+    const inputLength = e.target.value.length;
+    // console.log(inputLength)
+
+    const counter = 140 - inputLength;
+
+    $('.counter').text(counter);
+
+    if (counter < 0) {
+      $('.counter').css('color', 'red')
+    } else if (counter >= 0) {
+      $('.counter').css('color', 'black')
+    }
+  })
+});
